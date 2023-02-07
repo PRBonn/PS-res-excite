4d7a1752077d38494b1462274a17ff58a47e73dc
matteosodano <62264708+matteosodano@users.noreply.github.com>
2023-02-07 16:50:37 +0100

    Update build_model.py

--- src/build_model.py
+++ src/build_model.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+.. codeauthor:: Matteo Sodano <matteo.sodano@igg.uni-bonn.de>
+
 Part of this code is taken and adapted from:
 https://github.com/TUI-NICR/ESANet/blob/main/src/build_model.py
 """
