d8cd0d2abc174a5a8ca47d3a1212a08c62655dc6
matteosodano <62264708+matteosodano@users.noreply.github.com>
2023-02-07 16:39:15 +0100

    Update prepare_data.py

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,4 +1,9 @@
-# -*- coding: utf-8 -*-
+"""
+.. codeauthor:: Matteo Sodano <matteo.sodano@igg.uni-bonn.de>
+
+Parts of this code are taken and adapted from:
+https://github.com/TUI-NICR/ESANet/blob/main/src/prepare_data.py
+"""
 
 import os
 import pickle
