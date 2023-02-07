e0bca86ba7acf3ea91fad5afe5384fa467492026
matteosodano <62264708+matteosodano@users.noreply.github.com>
2023-02-07 16:51:14 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+.. codeauthor:: Matteo Sodano <matteo.sodano@igg.uni-bonn.de>
+
 Parts of this code are taken and adapted from:
 https://github.com/TUI-NICR/ESANet/blob/main/src/preprocessing.py
 """
